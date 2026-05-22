47d31ad1845679ac9957ded3fab85f66e3df17ba
henryliang <henryliang3027@gmail.com>
2026-05-22 16:57:43 +0800

    Enable scrolling in GlassTab to handle screen overflow

--- app/src/main/java/com/example/goodglasses/ui/tab/GlassTab.kt
+++ app/src/main/java/com/example/goodglasses/ui/tab/GlassTab.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,7 +61,8 @@ fun GlassTab(
     Column(
         modifier = Modifier
             .padding(start = 25.dp, end = 25.dp)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.Start
     ) {
 
